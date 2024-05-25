4cf6a09261ca93909e944442139ddb1444d2a3e9
lokytech5 <lokytech5@gmail.com>
2024-05-25 16:04:39 +0100

    Added smooth scroll to navbar and drawer

--- app/Home/pages.tsx
+++ app/Home/pages.tsx
@@ -5,8 +5,21 @@ import Resume from '../components/Resume'
 import About from '../components/About'
 import Projects from '../components/Projects'
 import Footer from '../components/Footer'
+import { smoothScrollTo } from '../utils/smoothScroll'
 
 const HomePage = () => {
+  const closeDrawer = () => {
+    const drawerToggle = document.getElementById('my-drawer-3') as HTMLInputElement;
+    if (drawerToggle) {
+      drawerToggle.checked = false;
+    }
+  };
+
+  const handleClick = (sectionId: string) => {
+    smoothScrollTo(sectionId);
+    closeDrawer();
+  };
+
   return (
     <main>
     <div className="drawer drawer-end">
@@ -31,18 +44,18 @@ const HomePage = () => {
         <label htmlFor="my-drawer-3" aria-label="close sidebar" className="drawer-overlay"></label>
         <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content flex flex-col justify-center items-center space-y-4 no-underline rounded-box">
           {/* Sidebar content here */}
-          <li className="text-center">
-            <a href="#home">Home</a>
-          </li>
-          <li className="text-center">
-            <a href="#resume">Resume</a>
-          </li>
-          <li className="text-center">
-            <a href="#projects">Projects</a>
-          </li>
-          <li className="text-center">
-            <a href="#about">About</a>
-          </li>
+         <li className="text-center">
+              <a className="cursor-pointer" onClick={() => handleClick('home')}>Home</a>
+            </li>
+            <li className="text-center">
+              <a className="cursor-pointer" onClick={() => handleClick('resume')}>Resume</a>
+            </li>
+            <li className="text-center">
+              <a className="cursor-pointer" onClick={() => handleClick('projects')}>Projects</a>
+            </li>
+            <li className="text-center">
+              <a className="cursor-pointer" onClick={() => handleClick('about')}>About</a>
+            </li>
         </ul>
       </div>
     </div>
