6ea5629deab51c680be0120f196b7d2e6f924fc9
lokytech5 <lokytech5@gmail.com>
2024-05-26 18:24:29 +0100

    Added ErrorAlert component and LoadingSpinner component

---
+++ app/components/LoadingSpinner.tsx
@@ -0,0 +1,10 @@
+const LoadingSpinner = () => {
+    return (
+      <div className="spinner-container">
+        <span className="loading loading-dots loading-md"></span>
+      </div>
+    );
+  };
+  
+  export default LoadingSpinner;
+  
