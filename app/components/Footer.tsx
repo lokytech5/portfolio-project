adfd76f9ddc96693869f9cee3870d9bb322166c7
lokytech5 <lokytech5@gmail.com>
2024-06-06 12:52:43 +0100

    update footer and navbar component to use different colour

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -5,7 +5,7 @@ import { motion } from 'framer-motion';
 const Footer = () => {
   return (
     
-    <footer className="footer footer-center p-10 bg-base-100 text-base-content rounded">
+    <footer className="footer footer-center p-10 bg-base-300 text-base-content rounded">
     <nav className="grid grid-flow-col gap-4">
       <a className="link link-hover">Home</a>
       <a className="link link-hover">Resume</a>
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -4,7 +4,7 @@ import { smoothScrollTo } from '../utils/smoothScroll'
 const Navbar = () => {
   return (
     <div className="fixed top-0 left-0 right-0 z-50 p-4">
-      <div className="navbar bg-base-100 rounded-lg shadow-lg overflow-hidden mx-auto">
+      <div className="navbar bg-accent rounded-lg shadow-lg overflow-hidden mx-auto">
         <label htmlFor="my-drawer-3" className="btn btn-ghost lg:hidden">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
