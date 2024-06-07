e8b9c45346e13c399e75f9395e8c29a3f0d3bf2a
lokytech5 <lokytech5@gmail.com>
2024-06-07 13:27:12 +0100

    Added custom hooks for resume

---
+++ app/hooks/useResumeData.ts
@@ -0,0 +1,39 @@
+import { useQuery } from '@tanstack/react-query';
+import apiClient from '../utils/apiClient';
+import { ResumeData } from '../components/types';
+
+const fetchResumeData = async (): Promise<ResumeData> => {
+    const [
+      programmingLanguages,
+      frameworks,
+      databases,
+      cloudTechnologies,
+      educations,
+      experiences
+    ] = await Promise.all([
+      apiClient.get('/programming-languages'),
+      apiClient.get('/frameworks'),
+      apiClient.get('/databases'),
+      apiClient.get('/cloud-technologies'),
+      apiClient.get('/educations'),
+      apiClient.get('/experiences')
+    ]);
+  
+    return {
+      programmingLanguages: programmingLanguages.data.data,
+      frameworks: frameworks.data.data,
+      databases: databases.data.data,
+      cloudTechnologies: cloudTechnologies.data.data,
+      educations: educations.data.data,
+      experiences: experiences.data.data
+    };
+  };
+  
+  const useResumeData = () => {
+    return useQuery<ResumeData, Error>({
+      queryKey: ['resumeData'],
+      queryFn: fetchResumeData,
+    });
+  };
+  
+  export default useResumeData;
