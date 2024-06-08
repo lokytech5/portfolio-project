b3c6a541a3794c3cb33d0ece63b3a86c3603175a
lokytech5 <lokytech5@gmail.com>
2024-06-08 08:34:03 +0100

    update next config for hashnode blog configuration and updated navbar to add blog link

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+module.exports = {
+    async rewrites() {
+      return [
+        {
+          source: "/blog",
+          destination: "https://between-brackets.vercel.app/blog",
+        },
+        {
+          source: "/blog/:path*",
+          destination: "https://between-brackets.vercel.app/blog/:path*",
+        },
+      ];
+    },
+  };
+  
